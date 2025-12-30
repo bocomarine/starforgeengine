caf15c1ff0af89f7ec62a525b58190db78547145
bocomarine <thomasanthonybaca@gmail.com>
2025-12-30 15:59:17 -0600

    Added File Structure, Added interface class for entrypoint to application. Begun work on making the application scalable.

--- StarForge/Renderer.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <stdio.h>
-#include <d3d11.h>
-#include <d3dx11.h>
-
-// include the Direct3D Library file
-#pragma comment (lib, "d3d11.lib")
---
+++ StarForge/application.cpp
@@ -0,0 +1,23 @@
+#include "pch.h"
+#include "application.h"
+
+Application::Application()
+{
+}
+
+Application::~Application()
+{
+}
+
+bool Application::Initialize()
+{
+	return false;
+}
+
+void Application::Update()
+{
+}
+
+void Application::Shutdown()
+{
+}
