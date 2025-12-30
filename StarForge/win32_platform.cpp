a73e61b6c2719bd08d62acf490f1311e9b497394
bocomarine <thomasanthonybaca@gmail.com>
2025-12-29 20:53:12 -0600

    Started Blank Window Project. Testing Over.

---
+++ StarForge/win32_platform.cpp
@@ -0,0 +1,13 @@
+#include <CoreWindow.h>
+
+
+int WinMain(
+	[in]           HINSTANCE hInstance,
+	[in, optional] HINSTANCE hPrevInstance,
+	[in]           LPSTR     lpCmdLine,
+	[in]           int       nShowCmd
+)
+{
+
+
+}
